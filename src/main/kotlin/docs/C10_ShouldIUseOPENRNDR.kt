dffbaf609f8a49539e17495fa1ecf9568fddc6bb
Yi donghoon <icq4ever@gmail.com>
2023-02-05 17:47:16 +0900

    kr translated

--- src/main/kotlin/docs/C10_ShouldIUseOPENRNDR.kt
+++ src/main/kotlin/docs/C10_ShouldIUseOPENRNDR.kt
@@ -11,51 +11,43 @@ import org.openrndr.dokgen.annotations.*
 fun main() {
     @Text
     """
-    # Should I use OPENRNDR ?
+    # OPENRNDR를 사용해야 할까요??
     
-    Yes!
+    그럼요!
     
-    .. but—
+    .. 하지만—
     
-    ## Target audience
+    ## 대상 
     
-    If you are new to both computer graphics and programming itself we suggest to first get your feet wet in the
-    _Processing_ language. Processing is specifically developed for the beginning programmer and offers a likely less steep
-    learning curve. That said, we believe OPENRNDR is developed with an aim on simplicity and consistency and should not be
-    super hard to learn. If you are experienced in Processing, you can find some
-    [posts](https://openrndr.discourse.group/search?q=%22Processing%22%20%23openrndr%3Atutorials)
-    in the forum comparing programs written in both environments.  
+    혹시 컴퓨터 그래픽 프로그래밍 경험이 처음이라면, 먼저 _Processing_ 언어를 접해보시는걸 추천드립니다. Processing은 프로그래밍을 처음 접하는 분들을 위해 디자인 되어있어서 학습커브가 얕습니다. 
+    즉, OPENRNDR은 단순함과 일관성에 촛점을 맞추었다고 믿고 있으므로 엄청 어렵진 않을겁니다. 혹시 Processing 경험이 있으시다구요? 그렇다면 프로세싱 포럼에서 프로세싱과 OPENRNDR을 비교하는 [이 게시글](https://openrndr.discourse.group/search?q=%22Processing%22%20%23openrndr%3Atutorials)을 읽어보시길 바랍니다. 
     
-    ## Supported platforms
+    ## 지원 플랫폼
     
-    OPENRNDR currently targets desktop platforms including macOS, Windows and Linux.
+    OPENRNDR 은 현재 맥OS, 윈도우, 리눅스를 포함한 데스크탑 플랫폼에서 동작합니다.
+
+    맥OS의 경우, 10.10에서 10.14까지를 지원합니다. 이전 버전은 동작할 수도 있지만, 확인하기 어렵습니다.
+
+    윈도우의 경우, 윈도우 10를 지원합니다. 윈도우 8.1에서는 동작할 수 있지만 확인되지 않았습니다.
+
+    리눅스의 경우, 우분두 18.04 LTS를 지원합니다. 다른 버전/배포판에서도 동작할 것입니다.
+
+    OPENRNDR은 최소 OpenGL 3.3을 지원하는 그래픽카드를 필요로 합니다. 이는 상대적으로 오래된 GPU인 nVidia 320M, intel HD4000을 포함합니다만 intel HD3000은 제외됩니다.
     
-    For macOS, we support versions 10.10 to 10.14. Older versions may work, but this is hard to verify in a structural 
-    fashion.
+    ## 지원되는 프로그래밍 언어
+
+    OPENRNDR은 코틀린으로 작성되었으며, JVM(자바 가상머신) 위에서 동작하도록 되어있습니다. 우리는 코틀린이 읽기 쉽고 잘 설명되는, 두가지 성격을 잘 갖춘 프로그래밍 언어라고 생각합니다.
+
+    라이브러리는 코틀린 Java-interop에 따라 Java 8 이상에서 사용될 수 있지만, OPENRNDR에서 제공되는 API들이 코틀린에 특화된 광범위한 사용을 가능하게 해주기 때문에 Java로 번역이 잘 되지 않을 수 있습니다.
+
     
-    For Windows, we support Windows 10. Windows 8.1 may work, but we have no means to verify this.
+    ## 지원되는 개발 환경
     
-    For Linux, we support Ubuntu 18.04 LTS. Other versions and distributions may work.
+    OPENRNDR은 특정 환경에 국한되지 않습니다만, 제공되는 모든 튜토리얼과 레퍼런스는 Gradel과 IntelliJ가 사용되었습니다.
     
-    OPENRNDR requires a graphics accelerator that supports at least OpenGL 3.3. This includes relatively old GPUs like
-    nVidia 320M, Intel HD4000 but excludes Intel HD3000 for example.
+    ## 장기적인 지원 
     
-    ## Supported programming languages
+    아직 안정화된 API를 약속할 단계에 다다르지는 않았습니다. OPENRNDR은 현재 pre-1.0 버전이며, 계획은 없지만, API의 불완정성, 혹은 불일치함을 유지하는것보다 변경하는것이 낫다고 판단되면 변경될 수 있습니다.
     
-    OPENRNDR is written in Kotlin and intended to run on the JVM. We believe Kotlin offers a well-balanced programming 
-    language that is both expressive and easy to read.
-    
-    The library can likely be used from Java 8+ as that's one of the promises of Kotlin's Java-interop, however the APIs
-    that OPENRNDR provides are making extensive use of Kotlin-specific features that may not translate well to Java.
-    
-    ## Supported development environments
-    
-    OPENRNDR is environment agnostic, however all our tutorial and reference material assumes Gradle and IntelliJ are used.
-    
-    ## Long-term support
-    
-    We have not reached the point at which we can make promises regarding API stability. OPENRNDR is pre-1.0 software, which
-    implies we try not to break things, but at times we have to. We break in cases in which it is better to break than to continue
-    with inconsistencies in or incompleteness of the API.
     """
 }
--- src/main/kotlin/docs/C20_SetupYourFirstProgram.kt
+++ src/main/kotlin/docs/C20_SetupYourFirstProgram.kt
@@ -11,83 +11,65 @@ import org.openrndr.dokgen.annotations.*
 fun main() {
     @Text
     """
-    # Getting Started with OPENRNDR
+    # OPENRNDR 시작하기
     
     Let's get it started!
     
-    ## Setting up software prerequisites
+    ## 환경 설정하기 
     
-    Now, to be able to edit and run OPENRNDR programs we have to install some 
-    software tools. 
+    OPENRNDR을 편집하고 실행하기 위해서는 몇가지 소프트웨어 도구를 설치해야 합니다.
     
-     * Check if your computer has `git` installed. If it's missing, install it 
-       from the [Git website](https://git-scm.com/) 
+     * `git`이 설치되어 있는지 확인하세요. 만약에 없다면 [Git website](https://git-scm.com/)에서 설치할 수 있습니다.
+       from the 
     
-     * Download [IntelliJ IDEA Community Edition](https://www.jetbrains.com/idea/download) 
-     and install it. On the first run it will offer the option to pick 
-     default settings and continue, use the default settings.
+     * [IntelliJ IDEA Community Edition](https://www.jetbrains.com/idea/download)를 다운받아 설치하세요.
+     처음 실행하면 기본설정을 제공할 수 있는데, 기본 설정을 사용하시기 바랍니다.
      
-    ## Clone the template
+    ## 템플릿 clone 하기
     
-    The advised and time-economic way to start any OPENRNDR-based project is to 
-    use the [`openrndr-template`](https://github.com/openrndr/openrndr-template).
-    This template contains a ready-to-go project from which you can quickly 
-    start running your first programs.
+    OPENRNDR 기반의 프로젝트를 간편하게 시작하기 위해 추천되는 방법은 [`openrndr-템플릿`](https://github.com/openrndr/openrndr-template)을 사용하는 것입니다.
+    이 템플릿은 첫 프로그램을 바로 실행할수 있는 프로젝트입니다.
     
-     * If program menus are visible select "File > New > Project from version control...".
-       If you are running Idea for the first time or no project is open, the program menus
-        are not visible. In that case click "Get from VCS" at the top-right area instead. 
-     * In the dialog that appears:
-       * Version Control: choose "Git".
-       * URL: enter `https://github.com/openrndr/openrndr-template`
-       * Directory: validate that it looks OK. 
-     * When asked where the project should be opened, click on "new window".
+     * IntelliJ Idea에서 메뉴가 보이신다면 "File > New > Project from version control..." 를 선택하세요.
+      만약 처음 실행하거나, 열린 프로젝트가 없다면, 프로그램 메뉴가 보이지 않을겁니다. 
+      우측 상단의  "Get from VCS" 를 클릭하세요.
+     * 다이얼로그에서 :
+       * Version Control: "Git"을 선택하세요.
+       * URL: `https://github.com/openrndr/openrndr-template`를 입력하세요
+       * Directory: 저장할 위치를 선택합니다.
+     * 프로젝트를 어디에서 열지 물어볼때, "new window"를 선택하세요.
     
-    If cloning fails, check if you have Git installed and if the repository URL 
-    was typed correctly.
+  
+    만약 clone이 실패한다면, Git이 설치되어있는지 확인하세요. 저장소 URL이 올바른지도 확인해주시기 바랍니다.
     
-    ## Run your first program
+    ## 첫 프로그램 실행하기 
     
-    After cloning the template you will likely see IntelliJ IDEA download 
-    dependencies and index your project, this may take some time on the 
-    first run. You can see its progress in the status bar at the bottom 
-    of the IntelliJ window.
-    
-    You may have noticed that IntelliJ has opened the `README.md` in the 
-    `openrndr-template` project. Likely this file will
-    contain some hints to help you navigate the template project. 
+    템플릿을 복제하면 IntelliJ IDEA가 의존성패키지를 다운받고 인덱싱 하는 걸 보실수 있습니다, 처음이라면 상당한 시간이 걸릴것입니다.
+    IntelliJ 창의 하단에 위치한 상태바에서 진행상황을 확인할 수 있습니다.
+       
+    아마 IntelliJ에서 `openrndr-template` 프로젝트의 `README.md`가 열려있는걸 알아차리신 분들이 있을겁니다. 템플릿 프로젝트에 관한 도움말이 담겨 있습니다.
+
+    IntelliJ 인덱싱이 끝나면, 프로젝트 뷰의 `src/main/kotlin/TemplateProgram.kt` 파일을 클릭하세요
+    IntelliJ의 버전에 따라 프로젝트뷰가 접혀있을 수 있습니다. 좌측의 세로 탭에서 프로젝트뷰를 클릭하여 열 수 있습니다.
      
-    Once IntelliJ has finished indexing 
-    click the `src/main/kotlin/TemplateProgram.kt` file in the Project view. 
-    On some versions of IntelliJ the Project view is collapsed by default, 
-    you can open it by clicking on the vertical tab on the left side of the screen. 
-     
-    Once the template program is opened a small green triangle should appear 
-    next to the line that starts with `fun main()`. Click on the triangle,
-    the program should now run.
+    템플릿 프로그램이 열리면, `fun main()` 옆에 작은 삼각형 버튼이 보일것입니다. 삼각형을 클릭하면 프로그램이 실행됩니다.
     
     *Lo' and behold!*
     
-    ## Tip for macOS users
+    ## 맥OS 사용자를 위한 팁
     
-    When running a program the console will display 
-    `Warning: Running on macOS without -XstartOnFirstThread JVM argument`.
-    To clear this warning (and enable debugging):
-    - Open the `Run > Edit Configurations...` menu.
-    - Add `-XstartOnFirstThread` in the `VM Options` [text field](https://stackoverflow.com/a/44184837).
-    - Click `Ok` to close the dialog.
+    프로그램을 선택하면 콘솔창에 `Warning: Running on macOS without -XstartOnFirstThread JVM argument` 경고 메시지가 뜰겁니다.
+    해당 경고를 없애려면 (디버깅 역시 활성화됩니다):
+    - 메뉴에서 `Run > Edit Configurations...`를 클릭하고 
+    - in the `VM Options`의 [text field](https://stackoverflow.com/a/44184837)에  `-XstartOnFirstThread`를 추가합니다. 
+    - `Ok`를 눌러 다이얼로그를 닫습니다.
     
-    ## What's next?
+    ## 그 다음은 뭐가 있죠?
     
-    At this point you are likely interested in how this program is structured. 
-    The guide explains more in the 
-    [Program basics](https://guide.openrndr.org/programBasics/applicationProgram.html) chapter.  
+    여기서 이 프로그앰의 구조가 어떻게 되는지 궁금하실겁니다.
+    [프로그램 기초](https://guide.openrndr.org/programBasics/applicationProgram.html) 챕터에서 이어집니다.
     
-    If you are more interested in reading source code you can find the code 
-    for the examples in this guide in the 
-    [`openrndr-examples` repository](https://github.com/openrndr/openrndr-examples). 
-    If you are interested in more advanced examples we recommend checking out 
-    the demo programs in the [`orx` repository](https://github.com/openrndr/orx), 
-    most `orx` modules have demos in the `src/demo/kotlin` folder.
+    소스코드가 어떻게 되는지 궁금하시다면, [`openrndr-examples` 저장소](https://github.com/openrndr/openrndr-examples). 에서 이 가이드에서 사용되는 예제코드들을 볼 수 있습니다. 
+    보다 고급 예제들이 궁금하시다면, [`orx` 저장소](https://github.com/openrndr/orx)를 살펴보시길 바랍니다. 대부분의 `orx` 모듈은 `src/demo/kotlin` 폴더에 데모를 갖고 있습니다.
     """
 }
