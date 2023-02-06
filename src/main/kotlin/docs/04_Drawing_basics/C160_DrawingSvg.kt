f4b0980e029993ec3fad10a359066cbbc0b90bd5
Yi donghoon <icq4ever@gmail.com>
2023-02-07 05:24:32 +0900

    kr translate

--- src/main/kotlin/docs/04_Drawing_basics/C160_DrawingSvg.kt
+++ src/main/kotlin/docs/04_Drawing_basics/C160_DrawingSvg.kt
@@ -18,9 +18,9 @@ import java.io.File
 fun main() {
     @Text 
     """
-    # Drawing SVG
+    # SVG 그리기
     
-    Loading a composition and drawing it can be done as follows:
+    컴포지션을 불러와 그것을 그려내는 방법은 아래와 같습니다:
     """
 
     @Code
@@ -35,16 +35,15 @@ fun main() {
 
     @Text 
     """
-    Note that OPENRNDR's support for SVG files works best with SVG files that are saved in the Tiny SVG 1.x profile .
+    OPENRNDR은 Tiny SVG 1.x 프로파일로 저장된 SVG파일과 가장 호환성이 좋습니다.    
     
-    
-    When drawing a composition from SVG you will notice that fill and stroke colors from the SVG file are used over the `Drawer` colors.
+    SVG에서 불러온 컴포지션을 그려낼때, `Drawer`의 색이 아닌, SVG파일에서 지정된 선과 fill의 색상이 사용되었음을 알 수 있습니다.
     
     ## Compositions
     
-    Here a `Composition` is a tree structure with `Composition.root` as its root node.
+    `Composition`은 `Composition.root`를 루트노드로 하는 트리 구조를 갖고 있습니다.
     
-    ##### CompositionNode types
+    ##### CompositionNode 타입들 
     
     Node Type         | Function
     ------------------|-----------------------------------------
@@ -54,7 +53,7 @@ fun main() {
     `ImageNode`       | Holds an image (currently not implemented)
     `CompositionNode` | The base class for composition node
     
-    ##### CompositionNode properties
+    ##### CompositionNode 프로퍼티
     
     Property name                | Property type      | Description
     -----------------------------|--------------------|------------
@@ -66,28 +65,29 @@ fun main() {
     `effectiveFill` (read-only)  | `ColorRGBa?`       | the effective fill color, potentially inherited from ancestor
     `effectiveStroke`(read-only) | `ColorRGBa?`       | the effective stroke color, potentially inherited from ancestor
     
-    ##### GroupNode properties
+    ##### GroupNode 프로퍼티
     
     Property name   | Property type                   | Description
     ----------------|---------------------------------|---------------
     `children`      | `MutableList<CompositionNode>`  | child nodes
     
-    ##### ShapeNode properties
+    ##### ShapeNode 프로퍼티
     
     Property name   | Property type  | Description
     ----------------|----------------|---------------
     `shape`         | `Shape`        | a single shape
     
-    ## Querying the composition
+    ## 컴포지션 조회하기
     
-    ### Finding all shapes in the composition
+    ### 컴포지션 내 모든 shape 찾기 
     
     ```kotlin
     val shapeNodes = composition.findShapes()
     ```
     
-    ## Modifying the composition
+    ## 컴포지션 수정
     
+    `Composition`은 여러 수정가능한 파트들을 갖고 있으므로, composition의 일부를 쉽게 교체 할 수 있습니다.
     Since a `Composition` contains many immutable parts it is easier to (partially) replace parts of the composition.
     
     ```kotlin
@@ -100,10 +100,11 @@ fun main() {
       }
     }
     ```
-    ## Creating compositions manually
+    ## 수동으로 컴포지션 생성하기
     
-    Compositions are tree structures that can be constructed manually. Below you find an example of constructing a `Composition` 
-    in code.
+    컴포지션은 트리구조로 되어있어 수동으로 생성할 수 있습니다. 
+    아래 코드는 `Composition`을 생성하는 방법을 보여주는 예제입니다.
+    
     """
 
     @Code.Block
@@ -114,18 +115,18 @@ fun main() {
         val shapeNode = ShapeNode(shape)
         shapeNode.fill = ColorRGBa.PINK
         shapeNode.stroke = ColorRGBa.BLACK
-        // -- add shape node to root
+        // -- 루트에 shape 노드를 추가합니다
         root.children.add(shapeNode)
     }
 
     @Text 
     """
-       ## Composition Drawer
-       
-       OPENRNDR has a much more convenient interface for creating Compositions. The idea behind this
-       interface is that it works in a similar way to `Drawer`. 
+    ## 컴포지션 Drawer
+    
+    OPENRNDR는 컴포지션을 생성하기 위한 훨씬 편리한 인터페이스를 제공합니다.
+    이 인터페이스는 `Drawer`와 유사한 방법으로 동작합니다.
 
-       Below we use `drawComposition {}` to reproduce the same composition as in the previous example.
+    아래는 `drawComposition {}`위 예제와 똑같은 컴포지션을 생성하는 코드입니다.
     """
 
     @Code.Block
@@ -139,9 +140,9 @@ fun main() {
 
     @Text 
     """
-    ### Transforms
+    # 좌표변환(Transforms)
         
-    Transforms work in the same way as in `Drawer`
+    좌표변환(Transforms) 역시 `Drawer`와 같은 방식으로 동작합니다.
     """
     @Code.Block
     run {
@@ -159,9 +160,9 @@ fun main() {
 
     @Text 
     """
-    Saving compositions to SVG
+    # 컴포지션을 SVG로 저장하기
     
-    Compositions can be saved to SVG using the `saveToFile` function.
+    컴포지션은 `saveToFile` 함수를 이용해 SVG 파일로 저장할 수 있습니다.
     """
 
     @Code.Block
