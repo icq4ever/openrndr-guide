fdbdad38b1399aa43dc4dcb6ae79594c029ab2d4
Yi donghoon <icq4ever@gmail.com>
2023-02-07 05:06:22 +0900

    "kr translate"

--- src/main/kotlin/docs/home.kt
+++ src/main/kotlin/docs/home.kt
@@ -16,19 +16,19 @@ fun main() {
     """
     # OPENRNDR이 뭐죠?
 
-    OPENRNDR is an application framework and a library for creative coding written in [Kotlin](https://kotlinlang.org).
+    OPENRNDR은 [Kotlin](https://kotlinlang.org)로 작성된 창의적 코딩을 위한 라이브러리이자, 어플리케이션 프레임워크입니다.
 
-    OPENRNDR offers APIs for easy and flexible programming of accelerated graphics.
+    OPENRNDR은 가속 그래픽 환경에서의 손쉽고 유연한 프로그래밍을 위한 API를 제공합니다.
 
-    OPENRNDR is intended for prototyping as well as building production quality software.
+    OPENRNDR은 프로토타이핑 뿐만 아니라, 프로덕션 레벨의 고품질 소프트웨어를 위한 도구입니다.
 
-    OPENRNDR is free and open source software. The source code can be found on [Github](https://github.com/openrndr/openrndr).
+    OPENRNDR은 무료 오픈소스 소프트웨어입니다. 소스코드는 [Github](https://github.com/openrndr/openrndr)에서 제공됩니다.
 
-    OPENRNDR is an initiative of the [RNDR](http://rndr.studio) studio for interactive and interaction design based in The Netherlands.
+    OPENRNDR은 네덜란드에 위치한 인터랙티브 및 인터랙션 스튜디오 [RNDR](http://rndr.studio)의 이니셔티브입니다.
 
 
-    # A simple OPENRNDR program
-    Here we show a very simple program written using OPENRNDR.
+    # 간단한 OPENRNDR 프로그램
+    아래는 OPENRNDR을 사용해 작성된 아주 간단한 프로그램입니다.
     """
 
     @Media.Video "media/what-is-001.mp4"
